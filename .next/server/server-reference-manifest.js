4ab0dbf7af438fc8952372261d046ccc6c8a0d0b
Samuel <samuelfidelis381@gmail.com>
2026-04-28 21:40:17 -0300

    chore: upgrade next@14.2.35 (security patch) and multer@2.1.1

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"egOCyCrU5Sl2bM9Y/q5Hq4dCUSSdEFgBuI2TXFRug2c=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
